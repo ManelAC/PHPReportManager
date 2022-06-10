99c3a8376d07ffe15217a276d3cbac4497c22e79
ManelAC <53198006+ManelAC@users.noreply.github.com>
2022-06-10 03:06:29 +0200

    Initial commit
    Initial project set up

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title>PHP Report Manager</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<link rel=stylesheet href="style.css" type="text/css" />
+</head>
+
+<body>
+	<div id="container">
+		<div id="header">
+			<h1>PHP Report Manager</h1>
+		</div>
+		<div id="leftcolumn">
+		</div>
+		<div id="body">
+			<p>This is the index of the application.</p>
+		</div>
+		<div id="footer">
+		</div>
+	</div>
+</body>
+</html>
